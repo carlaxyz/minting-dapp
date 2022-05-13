821505020950b6479a6d4c408727f46d058b0637
Rudolph Reyes <rudolph.reyes04@gmail.com>
2022-05-13 18:24:37 +0800

    -Changed Layer configs

--- backend/src/config.js
+++ backend/src/config.js
@@ -7,22 +7,30 @@ const { NETWORK } = require(`${basePath}/constants/network.js`);
 const network = NETWORK.eth;
 
 // General metadata for Ethereum
-const namePrefix = "YOUR COLLECTION NAME";
+const namePrefix = "SUGARCORE X";
 const description = "Remember to replace this description";
 const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 5,
+    growEditionSizeTo: 20,
     layersOrder: [
       { name: "Background" },
-      { name: "Eyeball" },
-      { name: "Eye color" },
-      { name: "Iris" },
-      { name: "Shine" },
-      { name: "Bottom lid" },
-      { name: "Top lid" },
+      { name: "Back" },
+      { name: "Base" },
+      { name: "Eyes" },
+      { name: "Mood" },
+      { name: "Blush" },
+      { name: "Mouth" },
+      { name: "Mask" },
+      { name: "Hair" },
+      { name: "Outfit" },
+      { name: "Glasses" },
+      { name: "Head" },
+      { name: "Chest" },
+      { name: "Bubble" },
+      { name: "Extra" },
     ],
   },
 ];
@@ -32,13 +40,13 @@ const shuffleLayerConfigurations = true;
 const debugLogs = false;
 
 const format = {
-  width: 512,
-  height: 512,
+  width: 4000,
+  height: 4000,
   smoothing: false,
 };
 
 const extraMetadata = {
-  external_url: "https://codecats.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://sugarcore.moe", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
